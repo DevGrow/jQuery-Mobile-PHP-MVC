3c4d0133c149c13c6d792f7a3af05ead3abdb30a
iriscrm <mnvx@yandex.ru>
2012-12-21 09:51:51 +0400

    Update lib/views/footer.php

--- lib/views/footer.php
+++ lib/views/footer.php
@@ -2,8 +2,8 @@
 	<div data-role="footer" data-position="fixed">
 		<p class="left p10">&copy; 2010 <?PHP echo APP_NAME; ?></p>
 		<ul class="right simplenav p10">
-			<li><a href="/site/about" data-icon="info" data-role="button">About</a></li>
-			<li><a href="/site/terms" data-icon="add" data-role="button">Terms of Service</a></li>
+			<li><a href="<?php echo BASE_URL; ?>/site/about" data-icon="info" data-role="button">About</a></li>
+			<li><a href="<?php echo BASE_URL; ?>/site/terms" data-icon="add" data-role="button">Terms of Service</a></li>
 		</ul>
 	</div>
 
